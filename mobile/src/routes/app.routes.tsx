150d8d1fa8d9c4ee724210af826420ed8b1babbc
Felipe Silva <felype.hen@hotmail.com>
2022-09-16 16:22:46 -0300

    connected with the API and made the page screen

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+import { Home } from '../screems/Home'
+import { Game } from '../screems/Game'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{
+      headerShown:false
+    }}>
+      <Screen 
+        name='home'
+        component={Home}
+      />
+      <Screen 
+        name='game'
+        component={Game}
+      />
+    </Navigator>
+  )
+}
