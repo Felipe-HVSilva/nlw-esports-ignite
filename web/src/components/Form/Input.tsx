a1e9b16898f2e852a8458203db521a8e34a07ebf
Felipe Silva <felype.hen@hotmail.com>
2022-09-17 14:24:45 -0300

    finished the web code

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -9,7 +9,6 @@ export function Input(props : InputProps) {
     <input 
         {...props}
         className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'  
-        type="text"
     />
   )
 }
